d071a9a1278f7a8b419417343ea5b108952b0990
junior procopio <juniorprocopio99@gmail.com>
2026-05-22 10:42:55 -0300

    novo app

--- src/services/api.js/api.js
+++ src/services/api.js/api.js
@@ -1,7 +1,31 @@
-import axios from 'axios';
+import { useEffect, useState } from 'react'
+import api from './services/api'
 
-const api = axios.create({
-  baseURL: 'https://sapataria-frontend.vercel.app/'
-});
+function App() {
+  const [mensagem, setMensagem] = useState('Carregando...')
 
-export default api;
+  useEffect(() => {
+    api.get('/')
+      .then((res) => {
+        setMensagem(res.data)
+      })
+      .catch(() => {
+        setMensagem('Erro ao conectar API')
+      })
+  }, [])
+
+  return (
+    <div style={{
+      padding: '40px',
+      fontFamily: 'Arial'
+    }}>
+      <h1>Sapataria App</h1>
+
+      <p>Status da API:</p>
+
+      <h2>{mensagem}</h2>
+    </div>
+  )
+}
+
+export default App
