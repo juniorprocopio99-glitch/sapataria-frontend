c830101aa28eb7233045adde15aa54bfb756ac02
junior procopio <juniorprocopio99@gmail.com>
2026-05-22 11:21:41 -0300

    app final

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import api from './services/api'
 
 function App() {
+
   const [mensagem, setMensagem] = useState('Carregando...')
 
   useEffect(() => {
