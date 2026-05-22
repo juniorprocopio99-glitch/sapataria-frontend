e830f33b37815be52e7a0dac9fae69512f81fc48
junior procopio <juniorprocopio99@gmail.com>
2026-05-22 11:02:16 -0300

    corrige api

--- src/services/api.js/api.js
+++ src/services/api.js/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://sapataria-frontend.vercel.app/'
+  baseURL: 'https://sapataria-backend.onrender.com'
 });
 
 export default api;
