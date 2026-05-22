5e5c0079a1cce6c9531ac73657fc44509d11dcea
junior procopio <juniorprocopio99@gmail.com>
2026-05-22 10:39:19 -0300

    conectando backend render

--- src/services/api.js/api.js
+++ src/services/api.js/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://sapataria-backend.onrender.com'
+  baseURL: 'https://sapataria-frontend.vercel.app/'
 });
 
 export default api;
